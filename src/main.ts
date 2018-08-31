4371ec88ac8daee494f66b3208b3649e3ca8ffda
Angel Bouzo <angel.bouzo@comcast.com>
2018-08-31 16:09:18 -0700

    app is in running state with canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
-const {app}
+const {app, BrowserWindow} = require("electron");
+
+class Main {
+    private window: typeof BrowserWindow;
+    private application: typeof app;
+
+    constructor(application: typeof app) {
+        this.application = application;
+
+        this.application.on("ready", this.createWindow);
+        this.application.on("windows-all-closed", this.onAllWindowsClosed);
+        this.application.on("activate", this.onActivate);
+    }
+
+    private createWindow() {
+        this.window = new BrowserWindow({width: 800, height: 600});
+        this.window.loadURL("file://" + __dirname + "/views/index.html");
+        this.window.on("closed", () => this.window = null);
+    }
+
+    private onAllWindowsClosed() {
+        if (process.platform !== "darwin") {
+            this.application.quit();
+        }
+    }
+
+    private onActivate() {
+        if (this.window === null) {
+            this.createWindow();
+        }
+    }
+}
+
+const main = new Main(app);
