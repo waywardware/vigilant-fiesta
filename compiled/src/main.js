90b120bfdcfc11136467685121e97a71114a5c1a
Angel Bouzo <38933611+ABouzo@users.noreply.github.com>
2018-08-31 17:24:45 -0700

    [Issue 1] There is nothing setup (#5)
    * app is in running state with canvas
    
    * adding files to gitignore

--- compiled/src/main.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
