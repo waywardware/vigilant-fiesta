9374d2ce4a75c321d04402b2fc04c5635816ae82
Angel Bouzo <angelb7574@gmail.com>
2018-10-18 16:11:40 -0700

    some rollup changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import typescript from "rollup-plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
 export default [
     {
@@ -8,6 +9,7 @@ export default [
             format: 'cjs'
         },
         plugins: [
+            terser(),
             typescript({
                 target: "ES6",
                 typescript: require("typescript")
@@ -22,6 +24,7 @@ export default [
             format: 'cjs'
         },
         plugins: [
+            terser(),
             typescript({
                 target: "ES6",
                 typescript: require("typescript")
